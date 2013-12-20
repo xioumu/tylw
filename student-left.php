007e6d065896644602cd92bff5ca9d49b7fb200f
xioumu <bwxioumu@gmail.com>
2013-12-20 19:23:35 +0800

    做完学生 前段
    做好student-pass, stdent-result

--- student-left.php
+++ student-left.php
@@ -10,8 +10,8 @@
 						<li class="nav-header hidden-tablet">菜单</li>
 						<li><a class="ajax-link" href="student-info.php"><span class="hidden-tablet">个人信息</span></a></li>
                         <li><a class="ajax-link" href="student-submit.php"><span class="hidden-tablet">提交论文</span></a></li>
-						<li><a class="ajax-link" href="form.html"><span class="hidden-tablet">查看结果</span></a></li>
-                        <li><a class="ajax-link" href="form.html"><span class="hidden-tablet">修改密码</span></a></li>
+						<li><a class="ajax-link" href="student-result.php"><span class="hidden-tablet">查看结果</span></a></li>
+                        <li><a class="ajax-link" href="student-passwd.php"><span class="hidden-tablet">修改密码</span></a></li>
 					</ul>
 				</div><!--/.well -->
 			</div><!--/span-->
